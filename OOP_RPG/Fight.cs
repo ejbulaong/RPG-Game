6eb484b463c8d215c19fa311dd924d88d9fc9939
ejbulaong <SD38@mitt.ca>
2019-02-19 15:09:20 -0600

    fulfilled requirement 3,4,5

--- OOP_RPG/Fight.cs
+++ OOP_RPG/Fight.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace OOP_RPG
@@ -13,25 +13,47 @@ namespace OOP_RPG
             Hero = game;
             Monsters = new List<Monster>();
 
-            AddMonster("Squid", 15, 5, 20);
+            var monstersGroup1 = new List<string> { "Trunks", "Tenshinhan", "Kuririn", "Yamcha", "Son Goku" };
+            var monstersGroup2 = new List<string> { "Android 18", "Son Goten", "Beerus", "Whis", "Son Gohan" };
+            var monstersGroup3 = new List<string> { "Taopaipai", "Broly", "Bobbidi", "Dabra", "Vegeta" };
+            var monstersGroup4 = new List<string> { "Oolong", "Pu'ar", "Chi-Chi", "Bulma", "Majin Boo" };
+            var monstersGroup5 = new List<string> { "Gyū-Maō", "Lunch", "Chaozu", "Dr. Briefs", "Cell" };
+            var monstersGroup6 = new List<string> { "Karin", "Yajirobe", "Mr. Popo", "Kami", "Freeza" };
+            var monstersGroup7 = new List<string> { "Dende", "Android 17", "Mr. Satan", "Videl", "Piccolo" };
+
+<<<<<<< HEAD
+            var daysOfWeek = new List<string> { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            var monstersGroups = new List<List<string>> { monstersGroup1, monstersGroup2, monstersGroup3, monstersGroup4, monstersGroup5, monstersGroup6, monstersGroup7 };
+            var difficulty = new List<string> { "Easy", "Easy", "Medium", "Medium", "Hard" };
+
+            for (var x = 0; x <= 6; x++)
+            {
+                if(DateTime.Today.DayOfWeek.ToString() == daysOfWeek[x])
+                {
+                    foreach(var name in monstersGroups[x])
+                    {
+                        AddMonster(name, difficulty[monstersGroups[x].IndexOf(name)]);
+                    }
+                }
+            }
         }
 
+        private void AddMonster(string name, string difficulty)
+        {
+            var monster = new Monster(name, difficulty);
+=======
         private void AddMonster(string name, int strength, int defense, int hp)
         {
-            var monster = new Monster();
-
-            monster.Name = name;
-            monster.Strength = strength;
-            monster.Defense = defense;
-            monster.OriginalHP = hp;
-            monster.CurrentHP = hp;
-
+            //Requirement 1: make it required that Name, Strength, Defense, OriginalHP, and CurrentHP parameters are provided when creating monsters across our code base.
+            var monster = new Monster(name, strength, defense, hp, hp);
+>>>>>>> cd75714f82f2ee9368f67590b3f64caa05b2e6fe
             Monsters.Add(monster);
         }
 
         public void Start()
         {
-            var enemy = Monsters[0];
+            Random rnd = new Random();
+            var enemy = Monsters[rnd.Next(0, 4)];
 
             while (enemy.CurrentHP > 0 && Hero.CurrentHP > 0)
             {
--- OOP_RPG/Monster.cs
+++ OOP_RPG/Monster.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace OOP_RPG
 {
     public class Monster
@@ -7,5 +9,43 @@ namespace OOP_RPG
         public int Defense { get; set; }
         public int OriginalHP { get; set; }
         public int CurrentHP { get; set; }
+<<<<<<< HEAD
+        public string Difficulty { get; }
+       
+        public Monster(string name, string difficulty)
+        {
+            if (difficulty == "Hard")
+            {
+                Strength = 16;
+                Defense = 6;
+                OriginalHP = 22;
+            }
+            else if(difficulty == "Medium")
+            {
+                Strength = 15;
+                Defense = 5;
+                OriginalHP = 20;
+            }
+            else if (difficulty == "Easy")
+            {
+                Strength = 14;
+                Defense = 4;
+                OriginalHP = 18;
+            }
+
+            Name = name;
+            CurrentHP = OriginalHP;
+=======
+
+        //Requirement 1: make it required that Name, Strength, Defense, OriginalHP, and CurrentHP parameters are provided when creating monsters across our code base.
+        public Monster(string name, int strength, int defense, int originalHP, int currentHP)
+        {
+            Name = name;
+            Strength = strength;
+            Defense = defense;
+            OriginalHP = originalHP;
+            CurrentHP = currentHP;
+>>>>>>> cd75714f82f2ee9368f67590b3f64caa05b2e6fe
+        }
     }
 }
